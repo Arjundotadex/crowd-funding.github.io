ebb6fbb9ccc350786921fbbcf964e027ae6546fb
Arjun <dotadexarjun2002@gmail.com>
2022-12-19 17:45:08 +0530

    added sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { logo, sun } from '../assets';
+import { navlinks } from '../constants';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
   return (
-    <div>Sidebar</div>
+    <div className='flex justify-between items-center flex-col sticky top-5 h-[93vh]'>
+      <Link to="/">
+      </Link>
+    </div>
   )
 }
 
