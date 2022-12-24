4417dd816864292698129547434b1ad86e10c96d
Arjun <dotadexarjun2002@gmail.com>
2022-12-24 14:56:58 +0530

    navbar optimised for mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,8 +34,26 @@ const Navbar = () => {
           </div>
         </Link>
       </div>
-      {/*Small Screen Navigation*/}
-       </div>
+      {/*Small screen navigation*/}
+      <div className='sm:hidden flex justify-between items-center relative'>
+        <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+          <img src={thirdweb} alt="user" className='w-[60%] object-contain' />
+        </div>
+        <img src={menu} alt="menu" className='w-[34px] h-[32px] object-contain cursor-pointer'
+          onClick={() => setToggleDrawer(!toggleDrawer)} />
+          <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer}? '-translate-y-[100vh]': 'translate-y-0]' transition-all duration-700`}>
+            <ul className='mb-4'>
+              {navlinks.map((link)=>(
+                <li>
+                  
+                </li>
+              ))}
+            </ul>
+
+          </div>
+      </div>
+
+    </div>
   )
 }
 
