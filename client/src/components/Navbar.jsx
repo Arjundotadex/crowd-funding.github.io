d84045394e424bb5b46e1d5dd712291811fdda53
Arjun <dotadexarjun2002@gmail.com>
2022-12-26 19:06:36 +0530

    navigation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,23 +34,32 @@ const Navbar = () => {
           </div>
         </Link>
       </div>
-      {/*Small screen navigation*/}
+      {/*Small Screen Navigation*/}
       <div className='sm:hidden flex justify-between items-center relative'>
         <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
           <img src={thirdweb} alt="user" className='w-[60%] object-contain' />
         </div>
         <img src={menu} alt="menu" className='w-[34px] h-[32px] object-contain cursor-pointer'
           onClick={() => setToggleDrawer(!toggleDrawer)} />
-          <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer}? '-translate-y-[100vh]': 'translate-y-0]' transition-all duration-700`}>
-            <ul className='mb-4'>
-              {navlinks.map((link)=>(
-                <li>
-                  
-                </li>
-              ))}
-            </ul>
+        <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer}? '-translate-y-[100vh]': 'translate-y-0]' transition-all duration-700`}>
+          <ul className='mb-4'>
+            {navlinks.map((link) => (
+              <li
+                key={link.name}
+                className={`flex p-4 ${isActive === link.name && 'bg-[#3a3a43]'}`}
+                onClick={() => {
+                  setIsActive(link.name);
+                  setToggleDrawer(false);
+                  navigate(link.link);
+                }}>
+                <img src={link.imgUrl} alt={link.name} />
+                <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive===link.name ? 'text-[#1dc071]'}`}>{link.name}</p>
 
-          </div>
+              </li>
+            ))}
+          </ul>
+
+        </div>
       </div>
 
     </div>
